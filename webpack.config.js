1de2d3fcd1b43fd2873f4c0957a18d2f333ee8ec
daugardas <daugismokykla@gmail.com>
2020-02-01 15:48:30 +0200

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ module.exports = {
   // webpack will take the files from ./src/index
   entry: './src/index',
 
-  // and output it into /dist as bundle.js
+  // and output it into /build as bundle.js
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, '/build'),
     filename: 'bundle.js'
   },
 
